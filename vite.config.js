04fda1978866dbfe1067f57e68ba9ca4ed299f9c
nromragm <mgarmorn@g.educaand.es>
2025-05-29 18:18:51 +0200

    ajustes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://noticias-ai-production.up.railway.app/',
+    base: process.env.ASSET_URL || '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/darkmode-boton.js'],
