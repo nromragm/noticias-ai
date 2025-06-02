9c7c47c20e7fa801926f1003a4b1b11c265d084c
nromragm <mgarmorn@g.educaand.es>
2025-06-02 22:58:08 +0200

    ajustes para deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     //base: process.env.ASSET_URL || '/',
+    base: '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/darkmode-boton.js', 'resources/js/darkmode-auto.js'],
